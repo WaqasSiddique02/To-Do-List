a2c8acb3d145d9a36822b8d015fdd4f0a39a954d
WaqasSiddique02 <siddique02mwaqas@gmail.com>
2024-07-03 16:06:34 +0500

    updated app.js and data.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express=require("express");
 const bodyParser=require("body-parser");
+const date=require(__dirname+"/date.js");
+
 const app=express();
 app.set('view engine','ejs');
 app.use(bodyParser.urlencoded({extended:true}));
@@ -9,15 +11,7 @@ var items=[];
 var workItems=[];
 
 app.get("/",function(req,res){
-    var today=new Date();
-
-    var options = {
-        weekday: "long",
-        day:"numeric" ,
-        month:"long"
-    }
-
-    var day=today.toLocaleDateString("en-US",options);
+   let day=date.getDate();
     res.render("list",{listTitle:day,newItems:items});
 });
 
